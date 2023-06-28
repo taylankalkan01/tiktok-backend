33e6d62f389c39a12541225b31e824a84c4d4bf0
taylankalkan01 <e.taylankalkan@hotmail.com>
2023-06-28 19:33:01 +0300

    fix: prettier format.

--- src/app.ts
+++ src/app.ts
@@ -12,7 +12,6 @@ import momentTimezone from "moment-timezone";
 
 // Custom Modules, Packages, Configs, etc.
 
-
 //Application
 const app: Application = express();
 app.set("trust proxy", 1);
@@ -25,7 +24,7 @@ app.use(helmet());
 app.use(compression());
 app.use(
   cors({
-    origin: ["http://localhost:3000",],
+    origin: ["http://localhost:3000"],
     credentials: true,
   }),
 );
@@ -39,6 +38,5 @@ app.use(
 app.get("/healthcheck", (_, res: Response) => {
   res.status(200).json({ error: false, message: "healthcheck" });
 });
-
 
 export default app;
