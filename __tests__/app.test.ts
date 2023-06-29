8ab6bb1e820d2deae802b847cee4730f86fbe005
taylankalkan01 <e.taylankalkan@hotmail.com>
2023-06-29 19:21:32 +0300

    fix: prettier script and formatted app.

--- __tests__/app.test.ts
+++ __tests__/app.test.ts
@@ -1,18 +1,17 @@
-import request from 'supertest';
-import app from '../src/app';
+import request from "supertest";
+import app from "../src/app";
 
-  describe('app healthcheck', () => {
-    it('should return status code 200',async () => {
-        const res = await request(app).get("/healthcheck");
-        expect(res.status).toBe(200);
-    });
-    it('should return healthcheck message',async () => {
-       const res =  await request(app).get("/healthcheck")    
-        expect(res.body.message).toBe("healthcheck");
-    });
-    it('should return json error:false ',async () => {
-       const res =  await request(app).get("/healthcheck")    
-        expect(res.body.error).toBe(false);
-    });
-
+describe("app healthcheck", () => {
+  it("should return status code 200", async () => {
+    const res = await request(app).get("/healthcheck");
+    expect(res.status).toBe(200);
   });
+  it("should return healthcheck message", async () => {
+    const res = await request(app).get("/healthcheck");
+    expect(res.body.message).toBe("healthcheck");
+  });
+  it("should return json error:false ", async () => {
+    const res = await request(app).get("/healthcheck");
+    expect(res.body.error).toBe(false);
+  });
+});
