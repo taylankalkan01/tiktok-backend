0e109609d1420e01945ebb3d1c8af5f0b9847879
taylankalkan01 <e.taylankalkan@hotmail.com>
2023-06-29 19:16:14 +0300

    Add unit tests for app.ts file

---
+++ __tests__/app.test.ts
@@ -0,0 +1,18 @@
+import request from 'supertest';
+import app from '../src/app';
+
+  describe('app healthcheck', () => {
+    it('should return status code 200',async () => {
+        const res = await request(app).get("/healthcheck");
+        expect(res.status).toBe(200);
+    });
+    it('should return healthcheck message',async () => {
+       const res =  await request(app).get("/healthcheck")    
+        expect(res.body.message).toBe("healthcheck");
+    });
+    it('should return json error:false ',async () => {
+       const res =  await request(app).get("/healthcheck")    
+        expect(res.body.error).toBe(false);
+    });
+
+  });
