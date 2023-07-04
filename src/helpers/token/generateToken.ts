ef8e9ced0f41c8c6ea9889be32547ffc5c95811a
taylankalkan01 <e.taylankalkan@hotmail.com>
2023-07-04 13:50:40 +0300

    add: generateToken helper function.

---
+++ src/helpers/token/generateToken.ts
@@ -0,0 +1,19 @@
+import User from "../../models/User";
+import jwt from "jsonwebtoken";
+
+export const genereateUserToken = (user: User) => {
+  try {
+    const payload = { userId: user._id };
+
+    const key = process.env.ACCESS_TOKEN_USER_KEY;
+    if (key === undefined || key === "" || key === null) {
+      return Promise.reject("Something wrong with token key");
+    }
+
+    const accessToken = jwt.sign(payload, key, {expiresIn: "3d"});
+
+    return Promise.resolve(accessToken);
+  } catch (error) {
+    return Promise.reject(error);
+  }
+};
