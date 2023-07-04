3cca340492666cf7dc74529d921c715e2f45b861
taylankalkan01 <e.taylankalkan@hotmail.com>
2023-07-05 02:39:47 +0300

    add: verifyUserToken helper function.

---
+++ src/helpers/token/verifyToken.ts
@@ -0,0 +1,11 @@
+import jwt from "jsonwebtoken"
+
+export const verifyUserToken = (token: string, key: string) => {
+    try {
+      key = process.env.ACCESS_TOKEN_USER_KEY!;
+      const verifyToken = jwt.verify(token, key);
+      return Promise.resolve(verifyToken);
+    } catch (error) {
+      return Promise.reject(error);
+    }
+  };
