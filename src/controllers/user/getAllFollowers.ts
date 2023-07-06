c900b2a98ec3645ff23d6ed94982f8fe537d383f
taylankalkan01 <e.taylankalkan@hotmail.com>
2023-07-06 19:42:34 +0300

    Implement pagination for getAllFollowers endpoint.

--- src/controllers/user/getAllFollowers.ts
+++ src/controllers/user/getAllFollowers.ts
@@ -5,6 +5,10 @@ import User from "../../models/User";
 export const getAllFollowers = async (req: Request, res: Response) => {
   const token = (req as CustomRequest).token;
   const userId = (token as { userId: string }).userId;
+  const { page, limit } = req.query;
+
+  const pageNumber = Number(page);
+  const pageSize = Number(limit);
 
   try {
     // Check if the user exists
@@ -13,8 +17,12 @@ export const getAllFollowers = async (req: Request, res: Response) => {
       return res.status(404).json({ message: "User not found" });
     }
 
+    //paginate
+    const skip = (pageNumber - 1) * pageSize;
     // Get the user's followers
-    const data = await User.find({ _id: { $in: user.followersIds } });
+    const data = await User.find({ _id: { $in: user.followersIds } })
+      .skip(skip)
+      .limit(pageSize);
 
     res.status(200).json({
       error: false,
